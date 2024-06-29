3205ea8edcebaedf061c9757564ed55dfc45ff16
MarzooqN <Marzooq.nadeem@gmail.com>
2024-06-29 12:53:30 -0400

    made modal better

--- OSUDegreePlanner/static/js/script.js
+++ OSUDegreePlanner/static/js/script.js
@@ -207,9 +207,9 @@ function prereqModalFunctionality(){
 /*
 Function to display prerequisites for a given course
 */
-function displayPrerequisites(courseId) {
+function displayPrerequisites(courseId, div = null) {
     const course = courseData.find(course => course.CourseID === courseId);
-    const resultDiv = document.getElementById('prerequisiteResult');
+    const resultDiv = div || document.getElementById('prerequisiteResult');
     resultDiv.innerHTML = '';  // Clear previous results
 
     if (course) {
@@ -917,6 +917,7 @@ async function checkAndAddCourse(selectElement, semesterTerm, semesterNum, cours
     //message modal and text
     const modal = document.getElementById('messageModal');
     const modalMessage = document.getElementById('message');
+    const modalDiv = document.getElementById('messageDiv');
     
     //Get selected course 
     const selectedCourseID = selectElement.value;
@@ -933,6 +934,7 @@ async function checkAndAddCourse(selectElement, semesterTerm, semesterNum, cours
     if (parseFloat(header.dataset.credits) + selectedCourse.Credits > 18){
         selectElement.value = "Click to Select Course"; // Reset selection
         modalMessage.textContent = 'Cannot add course: exceeds 18 credit hour limit';
+        modalDiv.innerHTML = ''
         openModal(modal);
         return;
     }
@@ -946,6 +948,7 @@ async function checkAndAddCourse(selectElement, semesterTerm, semesterNum, cours
     if(alreadySelected){
         selectElement.value = "Click to Select Course"; // Reset selection
         modalMessage.textContent = 'Cannot add course: Already selected in semester';
+        modalDiv.innerHTML = ''
         openModal(modal);
         return;
     }
@@ -982,6 +985,7 @@ async function checkAndAddCourse(selectElement, semesterTerm, semesterNum, cours
             allGroupsSatisfied = false;
             selectElement.value = "Click to Select Course"; // Reset selection
             modalMessage.textContent = `You have not met the prerequisites for ${selectedCourseID}`;
+            displayPrerequisites(selectedCourseID, modalDiv)
             openModal(modal);
             return;
         }
